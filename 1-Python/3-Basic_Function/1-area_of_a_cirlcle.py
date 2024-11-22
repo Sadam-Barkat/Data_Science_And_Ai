04ead754eb7e24dad050e09d498061be5964dcfe
Sadam Barkat <157804568+Sadam-Barkat@users.noreply.github.com>
2024-11-22 18:13:27 +0500

    Add files via upload

---
+++ 1-Python/3-Basic_Function/1-area_of_a_cirlcle.py
@@ -0,0 +1,5 @@
+def area(height, width):
+    area = height * width
+    return area
+
+print(area(12,3))
---
+++ 1-Python/3-Basic_Function/2-sum_of_two_numbers.py
@@ -0,0 +1,5 @@
+def sum(no1, no2):
+    sum = no1 + no2
+    return sum
+
+print(sum(2,3))    
